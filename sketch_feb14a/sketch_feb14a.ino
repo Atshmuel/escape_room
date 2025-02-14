2adc391ab6fcc2323fed7dad8806252814a4408b
Shmuel atar <shmuelatar@gmail.com>
2025-02-14 13:39:17 +0200

    Created wifi and client connection function

--- sketch_feb14a/sketch_feb14a.ino
+++ sketch_feb14a/sketch_feb14a.ino
@@ -1,20 +1,19 @@
-#include <DIYables_4Digit7Segment_74HC595.h>
 
-#define SCLK  D5  // The ESP8266 pin connected to SCLK
-#define RCLK  D6  // The ESP8266 pin connected to RCLK
-#define DIO   D7  // The ESP8266 pin connected to DIO
-
-DIYables_4Digit7Segment_74HC595 display = DIYables_4Digit7Segment_74HC595(CLK, DIO);
-// display.setNumber(1, 9);  // set 9 at the 1st digit
-
+void SendData(int val) {
+  HTTPClient http;
+  String dataURL = "";
+  dataURL += "missionCode=" + String(val);
+  http.begin(client, "http://55.55.55.55/api/?" + dataURL);
+  int httpCode = http.GET();
+  Serial.println(httpCode);
+  http.end();
+}
 
 void setup() {
+  wifiSetup();
   Serial.begin(9600);
-  display.clear();
-
 }
 
 void loop() {
-   display.loop();
-
+  wifiLoop();
 }
--- sketch_feb14a/wifi.ino
+++ sketch_feb14a/wifi.ino
@@ -1,16 +1,27 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <DIYables_4Digit7Segment_74HC595.h>
+
+#define LPIN D3  //LOCK PIN
+
+#define SCLK D5  // The ESP8266 pin connected to SCLK
+#define RCLK D6  // The ESP8266 pin connected to RCLK
+#define DIO D7   // The ESP8266 pin connected to DIO
+
+
+DIYables_4Digit7Segment_74HC595 display = DIYables_4Digit7Segment_74HC595(CLK, DIO);
 
 IPAddress apIP(55, 55, 55, 55);
 
-const char* ssid = "Server";
-const char* lockPassword = "111222";
+const char* ssid = "Escape_Room";
+char* gamePassword = "";
+int qCnt = 4;
 const char* password = "123123123";
+unsigned long wifiTime = 0;
 
 ESP8266WebServer server(80);
 
 void handleNotFound() {
-
   String message = "File Not Found \n \n";
   message += "URI: ";
   message += server.uri();
@@ -28,27 +39,101 @@ void handleNotFound() {
 }
 
 void handleRoot() {
-  server.send(200, "text/plain", html);
-}
+  String html = R"rawliteral(
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>חדר בריחה</title>
+    <style>
+        body{
+            display: flex;
+            justify-content: center;
+            height: 100dvh;
+        }
+        form{
+            text-align: center;
+            width: 30%;
+            height: 10rem;
+        }
+        .fail{
+            background-color: red;
+            height: fit-content;
+            font-size: 1.2rem;
+            padding: .5rem;
+        }
+
+        .currect{
+            background-color: greenyellow;
+            height: fit-content;
+            font-size: 1.6rem;
+            padding: .7rem;
+            text-align: center;
+        }
+    </style>
+    </head>
+  <body dir="rtl">)rawliteral";
+  html += "<form method='get'>";
+  html += "<h2>הכנס את הקוד על מנת לצאת מהחדר</h2>";
+  html += "<input type='text' name='password'>";
+  html += "<input type='button' value='שלח'>";
+
+  if (server.hasArg("password")) {
+    if (server.arg("password").length() != 4) {
+      html += "<p class='fail'>לא נכון, נסה שוב</p>";
+    } else {
+      if (server.arg("password") == gamePassword) {
+        digitalWrite(lockPin, false);
+        html = "<form method='get'>";
+        html += "<p class='currect'>הצלחת לצאת מהחדר ! <br> ניפגש בפעם הבאה</p>";
+      } else {
+        html += : "<p class='fail'>לא נכון, נסה שוב</p>";
+      }
+    }
+
+    html += "</form>";
+    html += "</body>";
+    html += "</html>";
+    server.send(200, "text/html", html);
+  }
+
+  void handleMissionComplited() {
+    if (server.hasArg("missionCode")) {
+      gamePassword = gamePassword + server.arg("missionCode");
+      display.setNumber(qCnt, server.arg("missionCode").toInt());
+      qCnt--;
+      server.send(200);
+    }else{
+      server.send(400);
+    }
+  }
 
 
-void wifiSetup() {
-  Serial.begin(9600);
-  WiFi.mode(WIFI_AP_STA);
-  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
-  WiFi.softAP(ssid, password);
+  void wifiSetup() {
+    Serial.begin(9600);
 
-  server.on("/", handleRoot);
-  server.onNotFound(handleNotFound);
+    WiFi.mode(WIFI_AP_STA);
+    WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+    WiFi.softAP(ssid, password);
 
-  Serial.print("AP IP address: ");
-  Serial.println(apIP);
+    server.on("/", handleRoot);
+    server.on("/api", handleMissionComplited);
+    server.onNotFound(handleNotFound);
 
-  server.begin();
-}
+    Serial.print("AP IP address: ");
+    Serial.println(apIP);
+    pinMode(lockPin, OUTPUT);
+    digitalWrite(lockPin, true);
+    wifiTime = millis();
+    display.clear();
+    server.begin();
+  }
 
-
-void wifiLoop() {
-  server.handleClient();
-  delay(100);
-}
+  void wifiLoop() {
+    if (millis() - wifiTime >= 10) {
+      wifiTime = millis();
+      server.handleClient();
+    }
+    display.loop();
+  }
