828cb97077e426771adad9b8aaa381bca7a22c83
advaohana <adva1230@gmail.com>
2025-02-18 23:26:52 +0200

    ControlLights function
    added function that reads the potentiometer value, calculates 80% of the brightness and dims the led for 2 seconds.

--- sketch_feb14a/sketch_feb14a.ino
+++ sketch_feb14a/sketch_feb14a.ino
@@ -1,20 +1,27 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 
+#define RedLed 1
+#define light A0
+
 WiFiClient client; 
 
 bool a = true;
 unsigned long awaitTime = 0;
+bool isDimmed= false;
 
 
 void setup() {
   Serial.begin(9600);
   wifiSetup();
   awaitTime = millis();
+  pinMode(RedLed, OUTPUT);
+
 }
 
 void loop() {
   wifiLoop();
+  ControlLights();
   if(a && millis() - awaitTime > 5000) {
     Serial.println("FETCH");
     SendData(7);
@@ -33,3 +40,21 @@ void SendData(int val) {
   Serial.println(httpCode);
   http.end();
 }
+
+void ControlLights(){
+  int potVal = map(analogRead(light), 0, 1023, 0, 255);
+  int dimLight= potVal*0.8;
+  if(!isDimmed){
+    analogWrite(RedLed,dimLight);
+    awaitTime= millis();
+    isDimmed= true;
+  }
+  if(isDimmed && millis()- awaitTime = 2000){
+    SendData(8);
+    analogWrite(RedLed,potVal);
+    isDimmed= false;
+  }
+
+}
+
+
